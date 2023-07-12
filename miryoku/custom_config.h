80790585ef8d54248c76079adc95fc8e91626d1c
Quentin <qlebastard@gmail.com>
2023-07-12 16:30:10 +0200

    base layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,6 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp B,             &kp W,             &kp P,             &kp O,             &kp SQT,             &kp DOT,             &kp V,             &kp D,             &kp L,             &kp J,             \
-U_MT(LGUI, Q),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, M),     \
-U_LT(U_BUTTON, W), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp COMMA,         &kp DOT,           U_MT(RALT, SLASH), U_LT(U_BUTTON, SQT),\
-U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, RET),  U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, ESC),U_NP,              U_NP
+U_MT(LGUI, A),     U_MT(LALT, U),     U_MT(LCTRL, I),    U_MT(LSHFT, E),    &kp COMMA,             &kp C,             U_MT(LSHFT, T),    U_MT(LCTRL, S),    U_MT(LALT, R),     U_MT(LGUI, N),     \
+U_LT(U_BUTTON, W), U_MT(RALT, Y),     &kp X,             &kp SLASH,             &kp K,             &kp M,             &kp Q,         &kp G,           U_MT(RALT, H), U_LT(U_BUTTON, F),\
+U_NP,              U_NP,              U_LT(U_FUN, BSPC),  U_LT(U_NUM, RET), U_LT(U_SYM, TAB),  U_LT(U_MOUSE, ESC),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, DEL),U_NP,              U_NP
