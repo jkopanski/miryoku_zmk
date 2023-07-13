c5d3fb1f676d26c57ba2ee786c9022b6cf39cded
Quentin <qlebastard@gmail.com>
2023-07-13 10:16:38 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,7 +12,7 @@ U_NP,              U_NP,              U_LT(U_FUN, BSPC),  U_LT(U_NAV, SPACE), U_
 
 #define MIRYOKU_LAYER_NAV \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LGUI,          &kp LALT,          &kp LSHFT,         &kp L,         U_NA,              &kp BSPC,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
+&kp LGUI,          &kp LALT,          &kp LSHFT,         &kp LCTRL,         U_NA,              &kp BSPC,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp DEL,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp RET,          &kp DEL,           U_NP,              U_NP
 
